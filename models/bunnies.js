1329d7d85d302c72091f583ad0b088d1b1ee881d
Ejike <reyhenry38@gmail.com>
2018-03-06 16:43:55 -0500

    Populated bunnies js

---
+++ models/bunnies.js
@@ -0,0 +1,20 @@
+// Bunnies models
+
+// The bunny has attributes of type DataTypes.String
+
+module.exports = function (sequelize, DataTypes) {
+
+    var Bunny = sequelize.define("Bunny", {
+        bunnyName: DataTypes.STRING,
+        destination: DataTypes.STRING,
+        age: DataTypes.STRING,
+        gender: DataTypes.STRING,
+        primaryLanguage: DataTypes.STRING,
+        secondaryLanguage: DataTypes.STRING,
+        activities: DataTypes.STRING,
+        bunnyPhoto: DataTypes.STRING,
+    });
+
+    return Bunny;
+
+};
