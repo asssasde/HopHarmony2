d8758241e306bb0a04a2b8e9d9067399303fdf7c
Ejike <reyhenry38@gmail.com>
2018-03-07 01:37:21 -0500

    configuring passport.js

---
+++ config/passport.js
@@ -0,0 +1,24 @@
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+
+const db = require("../models");
+
+passport.use( new localStrategy){
+    usernameField: "email"
+},
+
+function (email, password, done ){
+    db.User.findOne({
+        where: {
+            email:email
+        }
+    }).then((dbUser)=>{
+        //If the there is no user with the given email
+        if(!dbUser){
+            return done(numll, false), {
+                message:"Incorrect Email"
+            }
+        }
+    })
+
+}
