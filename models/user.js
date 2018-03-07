b9fe414feef7096e82f7ebaac26e7f3062f3d5b4
Ejike <reyhenry38@gmail.com>
2018-03-07 01:25:11 -0500

    User js

--- models/user.js
+++ models/user.js
@@ -19,10 +19,12 @@ module.exports = (sequelize, DataTypes) => {
     });
 
     User.prototype.validPassword = function(password){
-        return bcrypt.compareSync(password, this.password);
-
-        User.hook
+        return bcrypt.compareSync(password, this.password);  
     }
+    
+    User.hook("beforeCreate", function (user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    })
 
     return User;
     
