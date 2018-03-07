72c4fc088146684a0ac26340df184fe665a6d586
Ejike <reyhenry38@gmail.com>
2018-03-07 01:28:38 -0500

    created passport and middleware

--- models/user.js
+++ models/user.js
@@ -21,12 +21,11 @@ module.exports = (sequelize, DataTypes) => {
     User.prototype.validPassword = function(password){
         return bcrypt.compareSync(password, this.password);  
     }
-    
+
     User.hook("beforeCreate", function (user) {
         user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
     })
 
     return User;
-    
-
+ 
 }
